7ca5e450959d8fbe76ebc640c02ce085f9139f8d
KarinaShafa <karina_shafa_27rpl@student.smktelkom-mlg.sch.id>
2024-01-07 10:31:58 +0700

    tampilan UI

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -4,46 +4,45 @@ import streamlit as st
 # membaca model
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-# judul web
-st.title('Data Mining: Prediksi Diabetes')
+#judul web
+st.title('Data Mining Prediksi Diabetes')
 
-# membagi kolom
+#membagi kolom
 col1, col2 = st.columns(2)
 
-with col1:
-    Pregnancies = (st.text_input('Input nilai Pregnancies'))
+with col1 :
+    Pregnancies = st.text_input ('input nilai Pregnancies')
 
-with col2:
-    Glucose = (st.text_input('Input nilai Glucose'))
+with col2 :
+    Glucose = st.text_input ('input nilai Glucose')
 
-with col1:
-    BloodPressure = (st.text_input('Input nilai Blood Pressure'))
+with col1 :
+    BloodPressure = st.text_input ('input nilai Blood Pressure')
 
-with col2:
-    SkinThickness = (st.text_input('Input nilai Skin Thickness'))
+with col2 :
+    SkinThickness = st.text_input ('input nilai Skin Thickness')
 
-with col1:
-    Insulin = (st.text_input('Input nilai Insulin'))
+with col1 :
+    Insulin = st.text_input ('input nilai Insulin')
 
-with col2:
-    BMI = (st.text_input('Input nilai BMI'))
+with col2 :
+    BMI = st.text_input ('input nilai BMI')
 
-with col1:
-    DiabetesPedigreeFunction = (st.text_input('Input nilai Diabetes Pedigree Function'))
+with col1 :
+    DiabetesPedigreeFunction = st.text_input ('input nilai Diabetes Pedigree Function')
 
-with col2:
-    Age = (st.text_input('Input nilai Age'))
+with col2 :
+    Age = st.text_input ('input nilai Age')
 
-# code prediction untuk prediksi
+# code untuk prediksi
 diab_diagnosis = ''
 
-# membuat button prediksi
+# membuat tombol untuk prediksi
 if st.button('Test Prediksi Diabetes'):
     diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
-    if diab_prediction[0] == 1:
-        diab_diagnosis = 'Pasien terkena diabetes'
+    if(diab_prediction[0] == 0):
+        diab_diagnosis = 'Pasien terkena Diabetes'
     else:
-        diab_diagnosis = 'Pasien tidak terkena diabetes'
-
-    st.success(diab_diagnosis)
+        diab_diagnosis = 'Pasien tidak terkena Diabetes'
+st.success(diab_diagnosis)
